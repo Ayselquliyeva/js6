dc924a88cdf3e1df4db898c6ba8336bc6dd32645
Ayselquliyeva <92804962+Ayselquliyeva@users.noreply.github.com>
2021-12-10 14:07:25 +0400

    Add files via upload

---
+++ 9/script.js
@@ -0,0 +1,12 @@
+let saitler =[
+"a","ı","o","u","e","ə","i","ö","ü"
+]
+function say(){
+    let show =document.getElementById("show");
+    let inp =document.getElementById("inp");
+ 
+
+    for(let i =0;i<saitler.length;i++){
+   
+    }
+}
